6ffca9ab7a300aaf1b8ccdde5cec22f39f90d3bb
JohnPss <joaopedrosilvasiqueira1@gmail.com>
2024-07-17 21:40:06 -0300

    add new fuctions

--- lab04.c
+++ lab04.c
@@ -17,22 +17,54 @@ void ctdFrq(char *s)
 
 void tcldRplc(char *s)
 {
-    char str2[128] = "";
+    char str1[128] = "";
     char *plvrTpm = strtok(s, " ");
 
     while (plvrTpm != NULL)
     {
         if (strcmp(plvrTpm, "teclado") != 0)
         {
-            strcat(str2, plvrTpm);
-            strcat(str2, " ");
+            strcat(str1, plvrTpm);
+            strcat(str1, " ");
         }
         else
         {
-            strcat(str2, "teclado e mouse");
-            strcat(str2, " ");
+            strcat(str1, "teclado e mouse");
+            strcat(str1, " ");
         }
         plvrTpm = strtok(NULL, " ");
+    }
+
+    if (str1[0] != '\0')
+        str1[strlen(str1) - 1] = '\0';
+
+    printf("%s", str1);
+}
+
+void eclalnRplc(char *str)
+{
+    char str2[128] = "";
+    char *plvrstr = strtok(str, " ");
+
+    while (plvrstr != NULL)
+    {
+        if (strcmp(plvrstr, "aluno") != 0 && strcmp(plvrstr, "escola") != 0)
+        {
+            strcat(str2, plvrstr);
+            strcat(str2, " ");
+        }
+        else if (strcmp(plvrstr, "aluno") == 0)
+        {
+            strcat(str2, "estudante");
+            strcat(str2, " ");
+        }
+        else if (strcmp(plvrstr, "escola") == 0)
+        {
+            strcat(str2, "universidade");
+            strcat(str2, " ");
+        }
+
+        plvrstr = strtok(NULL, " ");
     }
 
     if (str2[0] != '\0')
@@ -41,16 +73,39 @@ void tcldRplc(char *s)
     printf("%s", str2);
 }
 
-int main()
+void mtzMtpl()
+{
+}
 
+int main()
 {
 
-    char s[128] = "";
+    int choice;
+    printf("Escolha uma opção:\n");
+    printf("1. ctdFrq\n");
+    printf("2. tcldRplc\n");
+    printf("3. eclalnRplc\n");
+    scanf("%d", &choice);
 
+    char input[128];
     printf("Digite uma sequencia de caracteres: ");
-    fgets(s, sizeof(s), stdin);
+    fgets(input, sizeof(input), stdin);
 
-    tcldRplc(s);
+    switch (choice)
+    {
+    case 1:
+        ctdFrq(input);
+        break;
+    case 2:
+        tcldRplc(input);
+        break;
+    case 3:
+        eclalnRplc(input);
+        break;
+    default:
+        printf("Opção inválida.\n");
+        break;
+    }
 
     // ctdFrq(s);
 }
