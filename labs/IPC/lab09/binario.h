d189e2d612c6049baad87aeb78b94c841abd5f91
JohnPss <joaopedrosilvasiqueira1@gmail.com>
2025-01-05 19:13:20 -0300

    att

---
+++ labs/IPC/lab09/binario.h
@@ -0,0 +1,9 @@
+#ifndef BINARIO_H
+#define BINARIO_H
+#include "autores.h"
+#include "documento.h"
+
+void salvarPilha(Pilha *p, char nomeArquivo[50]);
+void carregarPilha(Pilha *p, char nomeArquivo[50]);
+
+#endif // BINARIO_H
